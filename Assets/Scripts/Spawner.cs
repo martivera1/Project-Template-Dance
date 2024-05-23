bb22e12182df1fac37817d0330e5ce93b1b178fd
martivera1 <marti.vera01@estudiant.upf.edu>
2024-05-23 18:11:32 +0200

    song added + more arrows

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -10,14 +10,26 @@ public class Spawner : MonoBehaviour
     private Vector3 nextSpawnPosition;   
 
     public Vector3[] spawnPositions = {
-        new Vector3(75.0f, 0.0f, 25.0f),
-        new Vector3(80.0f, 0.0f, 25.0f),
-        new Vector3(90.0f, 0.0f, 25.0f),
-        new Vector3(120.0f, 0.0f, 25.0f),
-        new Vector3(150.0f, 0.0f, 25.0f)
+        new Vector3(55.26f, -1.567744f, 26.0f),
+        new Vector3(65.4f, -1.567744f, 35.0f),
+        new Vector3(85.0f, -1.567744f, 25.0f),
+        new Vector3(85.0f, -1.567744f, 34.8f),
+        new Vector3(65.0f,-1.567744f, 54.2f),
+        new Vector3(74.4f, -1.567744f, 65.1f),
+        new Vector3(65.6f,-1.567744f, 64.3f),
+        new Vector3(65.6f,-1.567744f, 84.7f)
+    };
+
+    public Vector3[] spawnRotations = {
+        new Vector3(90, 0, 0),
+        //new Vector3(90, 90, 0),
+        new Vector3(90, 180, 0),
+        new Vector3(90, 270, 0)
     };
 
     private int currentSpawnIndex = 0;
+    private int currentRotationIndex = 0;
+
 
     // Start is called before the first frame update
     void Start()
@@ -39,13 +51,14 @@ public class Spawner : MonoBehaviour
     void SpawnArrow()
     {
         Vector3 spawnPosition = spawnPositions[currentSpawnIndex];
-    
+        Vector3 spawnRotation = spawnRotations[currentRotationIndex];
 
         Quaternion rotation = Quaternion.Euler(spawnRotation);
         Instantiate(myPrefab, spawnPosition, rotation);
 
-
         currentSpawnIndex = (currentSpawnIndex + 1) % spawnPositions.Length;
+        currentRotationIndex = (currentRotationIndex + 1) % spawnRotations.Length;
+
 
     }
 
